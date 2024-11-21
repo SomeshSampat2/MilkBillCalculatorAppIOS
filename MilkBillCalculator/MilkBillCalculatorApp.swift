8210ade45100475157f71ea388066270421473f0
SomeshSampat2 <someshsampat1234@gmail.com>
2024-11-21 11:43:23 +0530

    Initial Commit

---
+++ MilkBillCalculator/MilkBillCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilkBillCalculatorApp.swift
+//  MilkBillCalculator
+//
+//  Created by Somesh Sampat on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MilkBillCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MilkBillCalculatorTests/MilkBillCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MilkBillCalculatorTests.swift
+//  MilkBillCalculatorTests
+//
+//  Created by Somesh Sampat on 21/11/24.
+//
+
+import Testing
+@testable import MilkBillCalculator
+
+struct MilkBillCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
